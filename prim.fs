05c96028898a76dd5b6a122d8c7d6d031079c4e5
benthor <github@benthor.name>
2016-05-09 14:00:48 +0200

    made formatting changes

--- prim.fs
+++ prim.fs
@@ -24,32 +24,32 @@ dup 0 > if      \ if we don't have any primes in the (remaining) sieve, stop
      rot drop drop true \ clean up after ourselves
   endif
 else
-   over dec.		\ this is were we actually report each found prime
-   rot drop drop false	\ clean up after ourselves
+  over dec.		\ this is were we actually report each found prime
+  rot drop drop false	\ clean up after ourselves
 endif
 ;
 
 : runhelper ( a-addr n1 n2 -- ) recursive \ n1 number of cells in a-addr, n2 number of already occupied cells
 2dup <> if     	     	\ break off the recursion if we have as many primes as cells
-   rot 2dup 2dup 2dup	\ make enough copies of address and cell number
-   swap	    	 	\ rearrange to allow for cell access
-   1 -			\ decrement index by 1
-   cells + @		\ read last cell
-   1 +			\ generate candidate from last cell
-   rot			\ rearrange arguments to befit checksieve
-   checksieve		\ check if our candidate is a multiple of a known prime or not
-   begin
-     0 <> while		\ while candidate isn't prime
-     1 +  		\ increment candidate by one
-     1 2over 2tuck 2drop drop	    \ FIXME, this is fucking ugly but required to copy our addresses and prime counts so far
-     rot		\ rearrange arguments to befit checksieve
-     checksieve		\ check if our candidate is a multiple of a known prime or not
-   repeat		\ repeat until we found an unknown prime
-   swap rot		\ arrange arguments so we can store it in the cell
-   cells + !		\ store
-   swap 1 + swap	\ increment number of found primes
-   rot rot  		\ rearrange arguments for recursive call
-   runhelper  		\ recursive call
+  rot 2dup 2dup 2dup	\ make enough copies of address and cell number
+  swap	    	 	\ rearrange to allow for cell access
+  1 -			\ decrement index by 1
+  cells + @		\ read last cell
+  1 +			\ generate candidate from last cell
+  rot			\ rearrange arguments to befit checksieve
+  checksieve		\ check if our candidate is a multiple of a known prime or not
+  begin
+    0 <> while		\ while candidate isn't prime
+    1 +  		\ increment candidate by one
+    1 2over 2tuck 2drop drop	    \ FIXME, this is fucking ugly but required to copy our addresses and prime counts so far
+    rot		\ rearrange arguments to befit checksieve
+    checksieve		\ check if our candidate is a multiple of a known prime or not
+  repeat		\ repeat until we found an unknown prime
+  swap rot		\ arrange arguments so we can store it in the cell
+  cells + !		\ store
+  swap 1 + swap	\ increment number of found primes
+  rot rot  		\ rearrange arguments for recursive call
+  runhelper  		\ recursive call
 endif
 ;
 
